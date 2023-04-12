8d4df692bd1ca5e08501290329c461f78d0b4d21
Daniele Fea <daniele.fea@vallauri.edu>
2023-04-12 12:31:22 +0200

    Decodifica ed elaborazione dei dati caricati

--- script.js
+++ script.js
@@ -20,7 +20,31 @@ function onbtnCarica(){
     let reader = new FileReader();
     //Indico alla libreria chi contattare terminata la lettura
     reader.onload =  function(datiletti){
-        console.log(datiletti);
+        //console.log(datiletti);// Oggetto FileReader
+        console.log(datiletti.currentTarget.result); //risultati codificati
+        let dati = datiletti.currentTarget.result.split("/");
+
+        //Trasformato da base64 utf8
+        let datiDecodificati = atob(dati[2]);
+
+        //Divido le righe del file in array
+        let righe = datiDecodificati.split("\r\n");
+
+        //Divido le colonne di ciascuna riga
+        /*
+            array di array
+
+            array esterno: righe del file
+            array interno: colonne di ciascuna riga
+        */
+
+        let record = [], colonne = [];
+        for(let riga of righe){
+            riga = riga.replaceAll("\"", "");
+            colonne = riga.split(",");
+            record.push(colonne);
+        }
+        console.log(record);
     };
 
     //Passo il file e avvio la lettura
